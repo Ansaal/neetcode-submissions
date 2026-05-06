bb055abcbaf014e391d09ec9ac09af5cbb2c3fe4
Ansaal <jhulsch.jh@googlemail.com>
2026-05-05 19:32:39 -0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.js
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const set = new Set();
+       nums.forEach(num=>set.add(num));
+       return set.size < nums.length;
+        
+    }
+}
